27242e97a774c69c4bffe8138fd720d8f90e8aa1
Marcos Tischer Vallim <tischer@gmail.com>
2017-06-20 01:52:13 -0300

    Add foo test

---
+++ src/test/foo.test.js
@@ -0,0 +1,10 @@
+describe("A test suite Foo", function () {
+  beforeEach(function () {
+  });
+  afterEach(function () {
+  });
+
+  it('should be  success', function () {
+    assert.equal(true, true);
+  });
+});
