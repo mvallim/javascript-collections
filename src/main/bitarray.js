d6a2b3f00adc90da73cf72a4c27fd81592e999df
Marcos Tischer Vallim <tischer@gmail.com>
2017-07-07 04:36:38 -0300

    Implementation BitArray
    Need improve performance in or, and, xor operations

--- src/main/bitarray.js
+++ src/main/bitarray.js
@@ -10,13 +10,42 @@ var BaseCollection = require('./commons/basecollection');
  */
 var BitArray = function BitArray(size) {
   BaseCollection.call(this, {
-    _size : size
+    _size : size,
+    _dataStore : []
   });
+
+  this.initDataStore();
 };
 
 BitArray.prototype = Object.create(BaseCollection.prototype, {
 
   constructor : BitArray,
+
+  /**
+   * @method
+   * @private
+   * @memberof BaseObject.prototype
+   */
+  initDataStore : {
+    value : function () {
+      for (var i = 0; i < this.chunks; i++) {
+        this._dataStore.push(this._ZERO);
+      }
+    },
+    enumerable : false,
+    configurable : false,
+    writable : false,
+  },
+
+  /**
+   * @private
+   */
+  _dataStore : {
+    value : undefined,
+    enumerable : false,
+    configurable : false,
+    writable : true,
+  },
 
   /**
    * @private
@@ -29,30 +58,60 @@ BitArray.prototype = Object.create(BaseCollection.prototype, {
   },
 
   /**
+   * @private
+   */
+  _ONES : {
+    get : function () {
+      return (Math.pow(2, this._WORD) - 1) >>> 0;
+    },
+    configurable : false
+  },
+
+  /**
+   * @private
+   */
+  _ZERO : {
+    get : function () {
+      return 0 >>> 0;
+    },
+    configurable : false
+  },
+
+  /**
+   * @private
+   */
+  _ONE : {
+    get : function () {
+      return 1 >>> 0;
+    },
+    configurable : false
+  },
+
+  /**
+   * @private
+   */
+  _WORD : {
+    get : function () {
+      return 32 >>> 0;
+    },
+    configurable : false
+  },
+
+  /**
    * @method
    * @memberof BitArray.prototype
    * @param {Number}
    *          index
-   * @param {Boolean}
-   *          bool
-   * @return {string}
    */
   set : {
-    value : function (index, bool) {
-    },
-    enumerable : false,
-    configurable : false,
-    writable : false,
-  },
+    value : function (index) {
+      var chunk = (index / this._WORD) >>> 0;
 
-  /**
-   * @method
-   * @memberof BitArray.prototype
-   * @param {Boolean}
-   *          bool 
-   */
-  setAll : {
-    value : function (bool) {
+      var position = (index % this._WORD) >>> 0;
+
+      var mask = this._ONE << position;
+
+      this._dataStore[chunk] = this._dataStore[chunk] | mask;
     },
     enumerable : false,
     configurable : false,
@@ -63,10 +122,62 @@ BitArray.prototype = Object.create(BaseCollection.prototype, {
    * @method
    * @memberof BitArray.prototype
    * @param {Number}
-   *          index 
+   *          index
+   */
+  unset : {
+    value : function (index) {
+      var chunk = (index / this._WORD) >>> 0;
+
+      var position = (index % this._WORD) >>> 0;
+
+      var mask = this._ONE << position;
+
+      mask = ~mask;
+
+      this._dataStore[chunk] = this._dataStore[chunk] & mask;
+    },
+    enumerable : false,
+    configurable : false,
+    writable : false,
+  },
+
+  /**
+   * @method
+   * @memberof BitArray.prototype
+   * @param {Boolean}
+   *          bool
+   */
+  setAll : {
+    value : function (bool) {
+      for (var i = 0; i < this.chunks; i++) {
+        this._dataStore[i] = bool ? this._ONES : this._ZERO;
+      }
+    },
+    enumerable : false,
+    configurable : false,
+    writable : false,
+  },
+
+  /**
+   * @method
+   * @memberof BitArray.prototype
+   * @param {Number}
+   *          index
+   * @return {Boolean}
    */
   get : {
     value : function (index) {
+      var chunk = (index / this._WORD) >>> 0;
+
+      var position = (index % this._WORD) >>> 0;
+
+      var mask = this._ONE << position;
+
+      if (this._dataStore[chunk] & mask) {
+        return true;
+      } else {
+        return false;
+      }
     },
     enumerable : false,
     configurable : false,
@@ -79,6 +190,9 @@ BitArray.prototype = Object.create(BaseCollection.prototype, {
    */
   not : {
     value : function () {
+      for (var i = 0; i < this.chunks; i++) {
+        this._dataStore[i] = ~this._dataStore[i];
+      }
     },
     enumerable : false,
     configurable : false,
@@ -94,6 +208,27 @@ BitArray.prototype = Object.create(BaseCollection.prototype, {
    */
   or : {
     value : function (bitarray) {
+      var high = this;
+      var low = bitarray;
+
+      if (bitarray.length > this.length) {
+        low = this;
+        high = bitarray;
+      }
+
+      var result = new BitArray(high.length);
+
+      for (var i = low.length - 1; i >= 0; i--) {
+        if (low.get(i)) {
+          result.set(high.length - 1 - (low.length - 1 - i));
+        }
+      }
+
+      for (var i = 0; i < high.chunks; i++) {
+        result._dataStore[i] = (result._dataStore[i] | high._dataStore[i]) >>> 0;
+      }
+
+      return result;
     },
     enumerable : false,
     configurable : false,
@@ -105,10 +240,31 @@ BitArray.prototype = Object.create(BaseCollection.prototype, {
    * @memberof BitArray.prototype
    * @param {BitArray}
    *          bitarray
-   * @return {BitArray} 
+   * @return {BitArray}
    */
   and : {
     value : function (bitarray) {
+      var high = this;
+      var low = bitarray;
+
+      if (bitarray.length > this.length) {
+        low = this;
+        high = bitarray;
+      }
+
+      var result = new BitArray(high.length);
+
+      for (var i = low.length - 1; i >= 0; i--) {
+        if (low.get(i)) {
+          result.set(high.length - 1 - (low.length - 1 - i));
+        }
+      }
+
+      for (var i = 0; i < high.chunks; i++) {
+        result._dataStore[i] = (result._dataStore[i] & high._dataStore[i]) >>> 0;
+      }
+      
+      return result;      
     },
     enumerable : false,
     configurable : false,
@@ -120,14 +276,51 @@ BitArray.prototype = Object.create(BaseCollection.prototype, {
    * @memberof BitArray.prototype
    * @param {BitArray}
    *          bitarray
-   * @return {BitArray} 
+   * @return {BitArray}
    */
   xor : {
     value : function (bitarray) {
+      var high = this;
+      var low = bitarray;
+
+      if (bitarray.length > this.length) {
+        low = this;
+        high = bitarray;
+      }
+
+      var result = new BitArray(high.length);
+
+      for (var i = low.length - 1; i >= 0; i--) {
+        if (low.get(i)) {
+          result.set(high.length - 1 - (low.length - 1 - i));
+        }
+      }
+
+      for (var i = 0; i < high.chunks; i++) {
+        result._dataStore[i] = (result._dataStore[i] ^ high._dataStore[i]) >>> 0;
+      }
+
+      return result;      
     },
     enumerable : false,
     configurable : false,
     writable : false,
+  },
+
+  /**
+   * @method
+   * @memberof BitArray.prototype
+   * @return {Number}
+   */
+  chunks : {
+    get : function () {
+      if (this.length % this._WORD == 0) {
+        return ((this.length / this._WORD) >>> 0);
+      } else {
+        return ((this.length / this._WORD) >>> 0) + 1;
+      }
+    },
+    configurable : false
   },
 
   /**
--- src/test/bitarray.test.js
+++ src/test/bitarray.test.js
@@ -8,20 +8,202 @@ describe('A test suite BitArray', function () {
   afterEach(function () {
   });
 
-  test('should be test call methods', function () {
-    const bitArray = new BitArray(1000);
+  test('should be set bit and value is setted', function () {
+    const bitArray = new BitArray(1000000);
 
-    bitArray.set(0, true);
-    bitArray.setAll(true);
-    bitArray.get(0);
-    bitArray.not();
-    bitArray.or(new BitArray(1000));
-    bitArray.and(new BitArray(1000));
-    bitArray.xor(new BitArray(1000));
-    bitArray.length;
-    bitArray.empty;
-    bitArray.clear();
-    
-    expect(bitArray.empty).toBe(true);
+    bitArray.set(0);
+
+    expect(bitArray.get(0)).toBe(true);
+
+    bitArray.set(31);
+
+    expect(bitArray.get(31)).toBe(true);
+
+    bitArray.set(32);
+
+    expect(bitArray.get(32)).toBe(true);
   });
+
+  test('should be unset bit and value is setted', function () {
+    const bitArray = new BitArray(1000000);
+
+    bitArray.set(0);
+    bitArray.unset(0);
+
+    expect(bitArray.get(0)).toBe(false);
+
+    bitArray.set(31);
+    bitArray.unset(31);
+
+    expect(bitArray.get(31)).toBe(false);
+
+    bitArray.set(32);
+    bitArray.unset(32);
+
+    expect(bitArray.get(32)).toBe(false);
+  });
+
+  test('should be setall bit to true and values are setted', function () {
+    const bitArray = new BitArray(100);
+
+    bitArray.setAll(true);
+
+    for (var i = 0; i < bitArray.length; i++) {
+      expect(bitArray.get(i)).toBe(true);
+    }
+  });
+
+  test('should be setall bit to false and values are setted', function () {
+    const bitArray = new BitArray(100);
+
+    bitArray.setAll(true);
+    bitArray.setAll(false);
+
+    for (var i = 0; i < bitArray.length; i++) {
+      expect(bitArray.get(i)).toBe(false);
+    }
+  });
+
+  test('should be setall values true and inverse all values', function () {
+    const bitArray = new BitArray(100);
+
+    bitArray.setAll(false);
+    bitArray.not();
+
+    for (var i = 0; i < bitArray.length; i++) {
+      expect(bitArray.get(i)).toBe(true);
+    }
+  });
+
+  test('should be setall values false and inverse all values', function () {
+    const bitArray = new BitArray(100);
+
+    bitArray.setAll(true);
+    bitArray.not();
+
+    for (var i = 0; i < bitArray.length; i++) {
+      expect(bitArray.get(i)).toBe(false);
+    }
+  });
+
+  test('should be or values wiht other bitarray', function () {
+    const bitArray01 = new BitArray(64);
+    const bitArray02 = new BitArray(100);
+
+    bitArray01.setAll(true);
+    bitArray02.setAll(false);
+
+    var or = bitArray01.or(bitArray02);
+
+    expect(or.length).toBe(Math.max(bitArray01.length, bitArray02.length));
+
+    for (var i = or.length - 1; i >= or.length - (or.length - Math.min(bitArray01.length, bitArray02.length)); i--) {
+      expect(or.get(i)).toBe(true);
+    }
+
+    bitArray01.setAll(false);
+
+    bitArray01.set(0);
+    bitArray01.set(31);
+    bitArray01.set(32);
+    bitArray01.set(63);
+
+    var or = bitArray01.or(bitArray02);
+
+    expect(or.length).toBe(Math.max(bitArray01.length, bitArray02.length));
+
+    expect(or.get(36)).toBe(true);
+    expect(or.get(67)).toBe(true);
+    expect(or.get(68)).toBe(true);
+    expect(or.get(99)).toBe(true);
+
+    for (var i = 0; i < or.length; i++) {
+      if (i != 36 && i != 67 && i != 68 && i != 99) {
+        expect(or.get(i)).toBe(false);
+      }
+    }
+  });
+  
+  test('should be and values wiht other bitarray', function () {
+    const bitArray01 = new BitArray(64);
+    const bitArray02 = new BitArray(100);
+
+    bitArray01.setAll(true);
+    bitArray02.setAll(false);
+
+    var and = bitArray01.and(bitArray02);
+
+    expect(and.length).toBe(Math.max(bitArray01.length, bitArray02.length));
+
+    for (var i = 0; i < and.length; i++) {
+      expect(and.get(i)).toBe(false);
+    }
+
+    bitArray01.setAll(false);
+    bitArray02.setAll(false);
+
+    bitArray01.set(0);
+    bitArray01.set(31);
+    bitArray01.set(32);
+    bitArray01.set(63);
+    
+    bitArray02.set(36);
+    bitArray02.set(67);
+    bitArray02.set(68);
+    bitArray02.set(99);
+
+    var and = bitArray01.and(bitArray02);
+
+    expect(and.length).toBe(Math.max(bitArray01.length, bitArray02.length));
+
+    expect(and.get(36)).toBe(true);
+    expect(and.get(67)).toBe(true);
+    expect(and.get(68)).toBe(true);
+    expect(and.get(99)).toBe(true);
+
+    for (var i = 0; i < and.length; i++) {
+      if (i != 36 && i != 67 && i != 68 && i != 99) {
+        expect(and.get(i)).toBe(false);
+      }
+    }
+  });
+  
+  test('should be xor values wiht other bitarray', function () {
+    const bitArray01 = new BitArray(64);
+    const bitArray02 = new BitArray(100);
+
+    bitArray01.setAll(true);
+    bitArray02.setAll(false);
+
+    var xor = bitArray01.xor(bitArray02);
+
+    expect(xor.length).toBe(Math.max(bitArray01.length, bitArray02.length));
+
+    for (var i = xor.length - 1; i >= xor.length - (xor.length - Math.min(bitArray01.length, bitArray02.length)); i--) {
+      expect(xor.get(i)).toBe(true);
+    }
+
+    bitArray01.setAll(false);
+    bitArray02.setAll(false);
+
+    bitArray01.set(0);
+    bitArray01.set(31);
+    bitArray01.set(32);
+    bitArray01.set(63);
+
+    var xor = bitArray01.xor(bitArray02);
+
+    expect(xor.length).toBe(Math.max(bitArray01.length, bitArray02.length));
+
+    expect(xor.get(36)).toBe(true);
+    expect(xor.get(67)).toBe(true);
+    expect(xor.get(68)).toBe(true);
+    expect(xor.get(99)).toBe(true);
+
+    for (var i = 0; i < xor.length; i++) {
+      if (i != 36 && i != 67 && i != 68 && i != 99) {
+        expect(xor.get(i)).toBe(false);
+      }
+    }
+  }); 
 });
